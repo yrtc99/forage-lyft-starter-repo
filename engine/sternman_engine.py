b4499b157fdd41c36c54119ea04236ec423b3140
yrtc99 <yrtc99@gmail.com>
2024-03-02 23:37:19 +0800

    refactor

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from engine.engine import Engine
 
 
@@ -7,19 +6,6 @@ class SternmanEngine(Engine):
         self.warning_light_is_on = warning_light_is_on
 
     def needs_service(self) -> bool:
-=======
-from abc import ABC
-
-from car import Car
-
-
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
-
-    def engine_should_be_serviced(self):
->>>>>>> 1c26f9cbad7b9bf859b3de73c4c65fb58e028a5f
         if self.warning_light_is_on:
             return True
         else:
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from engine.engine import Engine
 
 class WilloughbyEngine(Engine):
@@ -7,18 +6,4 @@ class WilloughbyEngine(Engine):
         self.last_service_mileage = last_service_mileage
 
     def needs_service(self):
-=======
-from abc import ABC
-
-from car import Car
-
-
-class WilloughbyEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
-        self.last_service_mileage = last_service_mileage
-
-    def engine_should_be_serviced(self):
->>>>>>> 1c26f9cbad7b9bf859b3de73c4c65fb58e028a5f
         return self.current_mileage - self.last_service_mileage > 60000
