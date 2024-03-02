b9ac6e2dc49576d19e87a51a4cf91d1dbb727f0b
yrtc99 <yrtc99@gmail.com>
2024-03-02 23:41:07 +0800

    car.py update

--- engine/car.py
+++ engine/car.py
@@ -2,9 +2,10 @@ from engine.engine import Engine
 from Battery.battery import Battery 
 from engine.serviceable import Serviceable
 
-def __init__(self, engine: Engine, battery: Battery):
-    self.engine = engine
-    self.battery = battery
+class Car(Serviceable):
+    def __init__(self, engine: Engine, battery: Battery):
+        self.engine = engine
+        self.battery = battery
 
-def needs_service(self) -> bool:
-    return self.engine.needs_service() or self.battery.needs_service()
+    def needs_service(self) -> bool:
+        return self.engine.needs_service() or self.battery.needs_service()
